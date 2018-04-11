5008a38f1ebf091ffaea4da7725651cd809cf815
Benjamin M. Hazelwood <benjnse@gmail.com>
2018-04-11 11:05:07 -0400

    change file references to point to location of text files

--- src/exampleauth.py
+++ src/exampleauth.py
@@ -3,8 +3,8 @@
 
 def exampleAuth():
     accountID, token = None, None
-    with open("account.txt") as I:
+    with open("../account.txt") as I:
         accountID = I.read().strip()
-    with open("token.txt") as I:
+    with open("../token.txt") as I:
         token = I.read().strip()
     return accountID, token
